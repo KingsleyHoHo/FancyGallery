c2ce3d7d21639e5f78a17f93602dbfa2db656471
BaoHoHo <96350422+BaoHoHo@users.noreply.github.com>
2025-04-19 19:00:41 -0400

    finished video week1

--- app/src/main/java/edu/vt/cs5254/fancygallery/MainActivity.kt
+++ app/src/main/java/edu/vt/cs5254/fancygallery/MainActivity.kt
@@ -2,10 +2,26 @@ package edu.vt.cs5254.fancygallery
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavHost
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import edu.vt.cs5254.fancygallery.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Standard from ADWS
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHost
+        binding.bottomNav.setupWithNavController(navHost.navController)
+
+        // require for navigation outside of bottom nav
+        binding.bottomNav.setOnItemSelectedListener { item ->
+            NavigationUI.onNavDestinationSelected(item, navHost.navController)
+            true
+        }
     }
 }
