168c1af788dbdc583cd3c5d367e9a2ab39fd9bfa
BaoHoHo <giabaoho123@gmail.com>
2025-05-01 17:19:03 -0400

    finish video phase2

--- app/src/main/java/edu/vt/cs5254/fancygallery/PhotoPageFragment.kt
+++ app/src/main/java/edu/vt/cs5254/fancygallery/PhotoPageFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebChromeClient
+import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import edu.vt.cs5254.fancygallery.databinding.FragmentPhotoPageBinding
@@ -21,12 +24,31 @@ class PhotoPageFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentPhotoPageBinding.inflate(inflater, container, false)
         binding.apply {
+
+            progressBar.max = 100
+
             webView.apply {
                 webViewClient = WebViewClient()
                 loadUrl(args.photoPageUri.toString())
+
+                webChromeClient = object : WebChromeClient() {
+                    override fun onReceivedTitle(view: WebView?, title: String?) {
+                        val parent = activity as AppCompatActivity?
+                        parent?.supportActionBar?.subtitle = title
+                    }
+
+                    override fun onProgressChanged(view: WebView?, newProgress: Int) {
+                        if (newProgress == 100){
+                            progressBar.visibility = View.GONE
+                        } else {
+                            progressBar.progress = newProgress
+                            progressBar.visibility = View.VISIBLE
+                        }
+                    }
+                }
             }
         }
         return binding.root
@@ -35,5 +57,8 @@ class PhotoPageFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+
+        val parent = activity as AppCompatActivity?
+        parent?.supportActionBar?.subtitle = null
     }
 }
